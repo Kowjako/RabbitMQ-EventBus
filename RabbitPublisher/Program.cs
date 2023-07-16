473d6a81b55ff6d5cc6186a674c7fe5807c25811
Wlodzimierz <kowyako@yandex.ru>
2023-07-16 16:00:29 +0200

    Completed eventbus

--- RabbitPublisher/Program.cs
+++ RabbitPublisher/Program.cs
@@ -3,12 +3,12 @@ using RabbitConsumer.EventBus;
 
 Console.WriteLine("Click to send msg to bus");
 Console.ReadKey();
+
 var bus = new RabbitMQBus();
 bus.Publish(new CreateOrderEvent()
 {
     Log = "Hello world"
 });
-
 
 Console.Read();
 
